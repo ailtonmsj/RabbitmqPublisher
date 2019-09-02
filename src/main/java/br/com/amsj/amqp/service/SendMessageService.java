55ea292f7deb79c48bb4abc991f80104899d37d7
Ailton Muniz Santos Junior <ailton.msj@gmail.com>
2019-09-01 23:14:59 -0300

    Initial commit

---
+++ src/main/java/br/com/amsj/amqp/service/SendMessageService.java
@@ -0,0 +1,25 @@
+package br.com.amsj.amqp.service;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class SendMessageService {
+	
+	@Autowired
+	RabbitTemplate rabbitTemplate;	
+	
+	final String EXCHANGE_NAME = "test_exchange_1";
+	final String ROUTING_KEY = "route_1";
+	
+	@RequestMapping(value="/send/", method=RequestMethod.POST)
+	public void sendMessage(@RequestBody String message) {
+		
+		rabbitTemplate.convertAndSend(EXCHANGE_NAME, ROUTING_KEY, message);
+	}
+}
