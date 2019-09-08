4587bc4a0b793fea1f0eac1d2bd1fe33622dfced
Ailton Muniz Santos Junior <ailton.msj@gmail.com>
2019-09-08 13:35:40 -0300

    Added configuration on .properties file

--- src/main/java/br/com/amsj/amqp/config/AmqpConfiguration.java
+++ src/main/java/br/com/amsj/amqp/config/AmqpConfiguration.java
@@ -2,24 +2,36 @@ package br.com.amsj.amqp.config;
 
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AmqpConfiguration {
 	
-	private static final String RABBITMQ_URL = "localhost";
-	private static final String RABBITMQ_USERNAME = "guest";
-	private static final String RABBITMQ_PASSWORD = "guest1";
+	public AmqpConfiguration(@Value("${amqp.host}") String rabbitmqHost, 
+			@Value("${amqp.port}") Integer rabbitmqPort, 
+			@Value("${amqp.username}") String rabbitmqUsername,
+			@Value("${amqp.password}") String rabbitmqPassword) {
+		
+		this.rabbitmqHost = rabbitmqHost;
+		this.rabbitmqPort = rabbitmqPort;
+		this.rabbitmqUsername = rabbitmqUsername;
+		this.rabbitmqPassword = rabbitmqPassword;
+	}
+	
+	private final String rabbitmqHost;
+	private final Integer rabbitmqPort;
+	private final String rabbitmqUsername;
+	private final String rabbitmqPassword;
 	
 	@Bean
 	ConnectionFactory connectionFactory() {
 		
-		CachingConnectionFactory cachingConnectionFactory = new CachingConnectionFactory(RABBITMQ_URL);
-		cachingConnectionFactory.setUsername(RABBITMQ_USERNAME);
-		cachingConnectionFactory.setPassword(RABBITMQ_PASSWORD);
+		CachingConnectionFactory cachingConnectionFactory = new CachingConnectionFactory(rabbitmqHost, rabbitmqPort);
+		cachingConnectionFactory.setUsername(rabbitmqUsername);
+		cachingConnectionFactory.setPassword(rabbitmqPassword);
 		
 		return cachingConnectionFactory;
 	}
-
 }
